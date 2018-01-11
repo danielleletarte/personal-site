0911d2ab33d36dbeb94f2a8a5a8bbbe808bea0bb
Letarte <n0213502@libp45p-10645dy.lm.lmig.com>
2018-01-11 13:41:02 -0500

    Added experience text and formatting.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import OnScreen from 'onscreen';
 
 $(document).ready( function() {
-    
+
     const os = new OnScreen({
         debounce: -5,
     });
@@ -10,43 +10,40 @@ $(document).ready( function() {
         var menu = document.getElementById('menu');
         menu.className = "menu-fixed";
     });
-    
+
     os.on('enter', '.landing-page', (element, event) => {
         var menu = document.getElementById('menu');
         menu.className = "";
     });
-    
+
     os.on('enter', '.about-page', (element, event) => {
         var about = document.getElementById('nav-about');
         about.className = "nav-about-active";
     });
-    
+
     os.on('leave', '.about-page', (element, event) => {
         var about = document.getElementById('nav-about');
         about.className = "";
     });
-    
+
     os.on('enter', '.experience-page', (element, event) => {
         var experience = document.getElementById('nav-experience');
         experience.className = "nav-experience-active";
     });
-    
+
     os.on('leave', '.experience-page', (element, event) => {
         var experience = document.getElementById('nav-experience');
         experience.className = "";
     });
-    
+
     os.on('enter', '.social-page', (element, event) => {
         var social = document.getElementById('nav-social');
         social.className = "nav-social-active";
     });
-    
+
     os.on('leave', '.social-page', (element, event) => {
         var social = document.getElementById('nav-social');
         social.className = "";
     });
-    
-});
-    
-    
 
+});
